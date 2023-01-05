7ba20c45aff3163b3e1850a63eebac92d82c35c0
GalloGabriel <gabriel.gallo2242@gmail.com>
2023-01-05 18:08:19 -0300

    iniciando projeto

---
+++ Documents/repos-app/src/pages/Main/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, Form, SubmitButton } from './styles';
+import { FaGithub, FaPlus } from 'react-icons/fa';
+
+export default function Main(){
+  return(
+    <Container>
+      
+      <h1>
+        <FaGithub size={25} />
+        Meus Repositórios
+      </h1>
+
+      <Form onSubmit={()=>{}}>
+        <input type="text" placeholder="Adicionar Repositórios" />
+
+        <SubmitButton>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+
+    </Container>
+  );
+}
---
+++ Documents/repos-app/src/pages/Repository/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Repository(){
+  return(
+    <div>
+      <h1>Repositórios</h1>
+    </div>
+  );
+}
